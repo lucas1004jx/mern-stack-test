ae3d98bed91c6cec3fd7ed4fe1882cca94d436ad
lucas1004jx <lucas1004jx@gmail.com>
2021-03-13 19:57:07 +0100

    set up storybook

---
+++ client/src/Nav/components/Navbar.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+export default {
+  title: 'Components/Button',
+  component: Navbar,
+};
+
+const Template = () => <Navbar />;
+export const Button = Template.bind({});
