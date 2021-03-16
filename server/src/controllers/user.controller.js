a824130483817020a50f5c7d8826a86fc16cd14c
lucas1004jx <lucas1004jx@gmail.com>
2021-03-16 09:30:23 +0100

    set up eslint server side

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,30 +1,30 @@
 
 
 const userSignIn = (req,res) => {
-console.log('user sign in', req.body.user)
-res.send('user sign in')
-}
+	console.log('user sign in', req.body.user);
+	res.send('user sign in');
+};
 
 const userSignUp = (req,res,next) => {
-console.log('user sign up', req.body.user)
-const {email,password,confirmedPassword} = req.body.user;
-const errors = [];
+	console.log('user sign up', req.body.user);
+	const {email,password,confirmedPassword} = req.body.user;
+	const errors = [];
 
-if(password !== confirmedPassword){
-errors.push('Password doesn\'t match')
-}
-if(password.length < 6){
-errors.push('The password should at least have 6 characters')
-}
+	if(password !== confirmedPassword){
+		errors.push('Password doesn\'t match');
+	}
+	if(password.length < 6){
+		errors.push('The password should at least have 6 characters');
+	}
 
-if(errors.length > 0) {
-return res.status(400).send({ message: errors.join() });
-}
+	if(errors.length > 0) {
+		return res.status(400).send({ message: errors.join() });
+	}
 
-res.status(200).send(req.body.user)
-}
+	res.status(200).send(req.body.user);
+};
 
 module.exports = {
-userSignIn,
-userSignUp
-}
+	userSignIn,
+	userSignUp
+};
