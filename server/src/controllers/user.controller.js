f49a2d11d374ac4bf84c83f7ea589ec51d06df03
lucas1004jx <lucas1004jx@gmail.com>
2021-03-16 09:24:20 +0100

    handle server side user sign up error

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -5,9 +5,23 @@ console.log('user sign in', req.body.user)
 res.send('user sign in')
 }
 
-const userSignUp = (req,res) => {
+const userSignUp = (req,res,next) => {
 console.log('user sign up', req.body.user)
-res.send('user sign up')
+const {email,password,confirmedPassword} = req.body.user;
+const errors = [];
+
+if(password !== confirmedPassword){
+errors.push('Password doesn\'t match')
+}
+if(password.length < 6){
+errors.push('The password should at least have 6 characters')
+}
+
+if(errors.length > 0) {
+return res.status(400).send({ message: errors.join() });
+}
+
+res.status(200).send(req.body.user)
 }
 
 module.exports = {
